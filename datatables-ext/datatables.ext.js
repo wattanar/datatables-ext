481e1b7aab66e119b1fabe84b189b63ab23e577f
Wattana Ruengmucha <wattanar@users.noreply.github.com>
2019-08-05 09:25:39 +0700

    Update datatables.ext.js

--- datatables-ext/datatables.ext.js
+++ datatables-ext/datatables.ext.js
@@ -1,34 +1,18 @@
-// @ts-nocheck
-function loadGrid(options) {
-  // initial filter row
-  $(options.el + " thead tr:eq(0)")
-    .clone()
-    .appendTo(options.el + " thead");
-  
-  if ($(options.el + " thead tr").length > 2) {
-    $(options.el + " thead tr")
-      .eq(2)
-      .remove();
-  }
+function loadGrid(o) {
+  $(o.el).DataTable(o);
 
-  var table = $(options.el).DataTable(options);
-
-  $(options.el + " thead tr:eq(1) th").each(function(i) {
-    if (
-      options.columns[i].filter === true ||
-      typeof options.columns[i].filter === "undefined"
-    ) {
-      $(this).html('<input type="text" style="width: 100%;" />');
-    } else {
-      $(this).html(
-        '<input type="text" readonly="true" style="width: 100%; background: #eeeeee; border: none;" />'
-      );
-    }
-
+  $(o.el + " thead tr:eq(1) th").each(function(i) {
+    $(this).html('<input type="text" style="width: 100%;" />');
     $("input", this).on("keyup change", function(e) {
-      if (table.column(i).search() !== this.value) {
+      if (
+        $(o.el)
+          .DataTable()
+          .column(i)
+          .search() !== this.value
+      ) {
         if (e.which === 13 || this.value === "") {
-          table
+          $(o.el)
+            .DataTable()
             .column(i)
             .search(this.value)
             .draw();
@@ -36,44 +20,14 @@ function loadGrid(options) {
       }
     });
   });
-
-  if (typeof options.modeSelect !== "undefined") {
-    if (options.modeSelect === "single") {
-      singleSelect(options.el);
-    } else if (options.modeSelect === "multiple") {
-      multipleSelect(options.el);
-    }
-  }
 }
 
-function singleSelect(selector) {
-  $(selector + " tbody").on("click", "tr", function() {
-    if (!$(this).hasClass("tb-selected")) {
-      $(selector)
-        .DataTable()
-        .$("tr.tb-selected")
-        .removeClass("tb-selected");
-      $(this).addClass("tb-selected");
-    }
-  });
-}
-
-function multipleSelect(selector) {
-  $(selector + " tbody").on("click", "tr", function() {
-    $(this).toggleClass("tb-selected");
-  });
-}
-
-function clearSelected(selector) {
-  $(selector + " tbody tr").removeClass("tb-selected");
-}
-
-function rowSelected(selector) {
+function getRowsSelected(selector) {
   var data = [];
 
   var row_selected = $(selector)
     .DataTable()
-    .rows(".tb-selected")
+    .rows(".selected", { selected: true })
     .data();
 
   if (typeof row_selected !== "undefined" && row_selected.length > 0) {
@@ -87,35 +41,14 @@ function rowSelected(selector) {
   }
 }
 
-function reloadGrid(selector, resetGrid = false) {
-  if (resetGrid === true) {
+function reloadGrid(selector, resetPage) {
+  if (typeof resetPage != "undefined") {
     $(selector)
       .DataTable()
-      .ajax.reload("null", true);
+      .ajax.reload(null, false);
   } else {
     $(selector)
       .DataTable()
       .ajax.reload();
   }
-}
-
-function rowDoubleClick(selector, el) {
-  return $(selector)
-    .DataTable()
-    .rows(el)
-    .data()[0];
-}
-
-function isSelectRow(rowdata) {
-  if (typeof rowdata === "undefined" || rowdata.length === 0) {
-    return false;
-  }
-  return true;
-}
-
-function isNull(data) {
-  if (data === null || data === "NULL") {
-    return "";
-  }
-  return data;
 }
